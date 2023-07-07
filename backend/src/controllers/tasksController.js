a3dd48e10d8755bda817569a43e17889772491dc
Vitor Barreto Fernandes <vitorbarreto404@gmail.com>
2023-07-07 02:35:31 -0300

    testando request
    testando as requisições enviadas ao banco de dados.

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -1,5 +1,8 @@
-const getAll = (request, response) => {
-    return response.status(200).json( { message: 'controller ok!' } );
+const tasksModel = require('../models/tasksModel');
+
+const getAll = async (request, response) => {
+    const  tasks = await tasksModel.getAll();
+    return response.status(200).json(tasks);
 };
 
 module.exports = {
--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,11 +1,11 @@
-const mysql = require('mysql/promise');
+const mysql = require('mysql');
+require('dotenv').config();
 
-
-const connection = mysql.createPoo({
-    host: 'process.env.MYSQL_HOST',
-    user: 'process.env.MYSQL_USER',
-    password: 'process.env.MYSQL_PASSWORD',
-    database: 'process.env.MYSQL_DB',
+const connection = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
 });
 
 module.exports = connection;
